eddddc900d770c312d79bb212190c6844e87a908
Elliot Smith <esmith2642@gmail.com>
2014-10-30 22:51:34 -0500

    Alfred edits.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -6,54 +6,54 @@ losses = 0
 ties = 0
 
 def game(wins, losses, ties):
-    print "Make your choice:"
+    print "Make your choice! Rock, Paper or Scissors?"
     player_choice = raw_input("=> ")
     computer_choice = randint(0,2)
     
-    if player_choice == "Rock":
-	    if computer_choice == 0:
-		    print "The computer played Rock, its a tie!"
-		    ties += 1
-		    end(wins, losses, ties)
-	    elif computer_choice == 1:
-		    print "The computer played Paper, you lose!"
-		    losses += 1
-		    end(wins, losses, ties)
-	    elif computer_choice == 2:
-		    print "The computer played Scissors, you win!"
-		    wins += 1
-		    end(wins, losses, ties)
+    if player_choice.lower() == "rock":
+        if computer_choice == 0:
+            print "The computer played Rock, its a tie!"
+            ties += 1
+            return end(wins, losses, ties)
+        elif computer_choice == 1:
+            print "The computer played Paper, you lose!"
+            losses += 1
+            return end(wins, losses, ties)
+        elif computer_choice == 2:
+            print "The computer played Scissors, you win!"
+            wins += 1
+            return end(wins, losses, ties)
 
-    if player_choice == "Paper":
-	    if computer_choice == 0:
-		    print "The computer played Rock, you win!"
-		    wins += 1
-		    end(wins, losses, ties)
-	    elif computer_choice == 1:
-		    print "The computer played Paper, its a tie!"
-		    ties += 1
-		    end(wins, losses, ties)
-	    elif computer_choice == 2:
-		    print "The computer played Scissors, you lose!"
-		    losses += 1
-		    end(wins, losses, ties)
-		
-    if player_choice == "Scissors":
-	    if computer_choice == 0:
-		    print "The computer played Rock, you lose"
-		    losses += 1
-		    end(wins, losses, ties)
-	    elif computer_choice == 1:
-		    print "The computer played Paper, you win!"
-		    wins += 1
-		    end(wins, losses, ties)
-	    elif computer_choice == 2:
-		    print "The computer played Scissors, its a tie!"
-		    ties += 1
-		    end(wins, losses, ties)
+    if player_choice.lower() == "paper":
+        if computer_choice == 0:
+            print "The computer played Rock, you win!"
+            wins += 1
+            return end(wins, losses, ties)
+        elif computer_choice == 1:
+            print "The computer played Paper, its a tie!"
+            ties += 1
+            return end(wins, losses, ties)
+        elif computer_choice == 2:
+            print "The computer played Scissors, you lose!"
+            losses += 1
+            return end(wins, losses, ties)
+        
+    if player_choice.lower() == "scissors":
+        if computer_choice == 0:
+            print "The computer played Rock, you lose"
+            losses += 1
+            return end(wins, losses, ties)
+        elif computer_choice == 1:
+            print "The computer played Paper, you win!"
+            wins += 1
+            return end(wins, losses, ties)
+        elif computer_choice == 2:
+            print "The computer played Scissors, its a tie!"
+            ties += 1
+            return end(wins, losses, ties)
     else:
-	    print "Invalid choice! Try again."
-	    game(wins, losses, ties)
+        print "Invalid choice! Try again."
+        return game(wins, losses, ties)
             
 def end(wins, losses, ties):
     print "Player wins: %d" % wins
@@ -63,8 +63,8 @@ def end(wins, losses, ties):
     print "Play again...?"
     answer = raw_input("=> ")
     
-    if answer == "Yes" or "yes" or "Y" or "y":
-	    game(wins, losses, ties)
+    if answer.lower() in ('yes','y'):
+        game(wins, losses, ties)
     else:
         sys.exit()
 
